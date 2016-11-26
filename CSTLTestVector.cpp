5e680b450185ca3df6e47eb9aac9a299d6d4546e
hidenorly <twitte.harold@gmail.com>
2016-11-27 00:30:43 +0900

    Add 2D vector & transpose utility with template

--- CSTLTestVector.cpp
+++ CSTLTestVector.cpp
@@ -41,18 +41,70 @@ void CSTLTestVector::cleanup(void) {
 
 }
 
+
+template <typename T>
+class vector2dUtil
+{
+public:
+	static std::vector<std::vector<T>> transpose(std::vector<std::vector<T>>& inData)
+	{
+		if(inData.size()!=0){
+			std::vector<std::vector<T>> outData( inData[0].size(), std::vector<T>(inData.size()) );
+			for(int x=0; x<inData.size(); x++){
+				for(int y=0; y<inData[x].size(); y++){
+					outData[y][x] = inData[x][y];
+				}
+			}
+			return outData;
+		} else {
+			std::vector<std::vector<T>> tmp;
+			return tmp;
+		}
+	}
+};
+
+
 void CSTLTestVector::test(void) {
-    // get size
-    std::cout << "size = " << mContainer.size() << std::endl;
+	// get size
+	std::cout << "size = " << mContainer.size() << std::endl;
 
-    // iterator with explicit type define
-    for(std::vector<int>::iterator it=mContainer.begin(); it!=mContainer.end(); it++){
-        std::cout << *it << std::endl;
-    }
+	// iterator with explicit type define
+	for(std::vector<int>::iterator it=mContainer.begin(); it!=mContainer.end(); it++){
+		std::cout << *it << std::endl;
+	}
 
-    // iterator with auto
-    for(auto it = mContainer.begin(); it!=mContainer.end(); it++) {
-        std::cout << *it << std::endl;
-    }
+	// iterator with auto
+	for(auto it = mContainer.begin(); it!=mContainer.end(); it++) {
+		std::cout << *it << std::endl;
+	}
+
+	std::vector<int> v{0};
+	std::vector<int> v2{503,502,501};
+	v.insert(v.begin(), v2.begin(), v2.end()); // insert v2 for head of v.
+	for( auto& item : v ){
+		std::cout << item << std::endl; // 503,502,501,0
+	}
+
+	// 2 dimensions vector [10][20] with 0 initial value
+	std::vector< std::vector<int> > xyMap(10, std::vector<int>(20,0));
+	for(int x=0; x<xyMap.size(); x++){
+		for(int y=0; y<xyMap[0].size(); y++){
+			xyMap[x][y]=y;
+		}
+	}
+	for(auto& aLine : xyMap ){
+		for(auto& aVal : aLine ) {
+			std::cout << std::to_string(aVal) << ",";
+		}
+		std::cout << std::endl;
+	}
+
+	for(auto& aLine : vector2dUtil<int>::transpose(xyMap)){
+		for(auto& aVal : aLine ) {
+			std::cout << std::to_string(aVal) << ",";
+		}
+		std::cout << std::endl;
+	}
 }
+
 
